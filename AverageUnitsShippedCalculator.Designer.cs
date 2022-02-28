266cce1182bba0895d66ffdfc3ac487b8bfd8a7e
Stevenson Suhardy <stevenson.suhardy@dcmail.ca>
2022-02-28 09:45:54 -0500

    Add project files.

---
+++ AverageUnitsShippedCalculator.Designer.cs
@@ -0,0 +1,166 @@
+﻿
+namespace AverageUnitsShippedCalculator
+{
+    partial class formAverageUnitsShipped
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.labelUnitsPrompt = new System.Windows.Forms.Label();
+            this.labelDay = new System.Windows.Forms.Label();
+            this.buttonEnter = new System.Windows.Forms.Button();
+            this.buttonReset = new System.Windows.Forms.Button();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.textBoxUnitsInput = new System.Windows.Forms.TextBox();
+            this.textBoxPastEntries = new System.Windows.Forms.TextBox();
+            this.labelAverage = new System.Windows.Forms.Label();
+            this.toolTip = new System.Windows.Forms.ToolTip(this.components);
+            this.SuspendLayout();
+            // 
+            // labelUnitsPrompt
+            // 
+            this.labelUnitsPrompt.AutoSize = true;
+            this.labelUnitsPrompt.Location = new System.Drawing.Point(52, 29);
+            this.labelUnitsPrompt.Name = "labelUnitsPrompt";
+            this.labelUnitsPrompt.Size = new System.Drawing.Size(37, 15);
+            this.labelUnitsPrompt.TabIndex = 0;
+            this.labelUnitsPrompt.Text = "&Units:";
+            this.labelUnitsPrompt.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // labelDay
+            // 
+            this.labelDay.AutoSize = true;
+            this.labelDay.Location = new System.Drawing.Point(203, 29);
+            this.labelDay.Name = "labelDay";
+            this.labelDay.Size = new System.Drawing.Size(36, 15);
+            this.labelDay.TabIndex = 2;
+            this.labelDay.Text = "Day 1";
+            // 
+            // buttonEnter
+            // 
+            this.buttonEnter.Location = new System.Drawing.Point(12, 277);
+            this.buttonEnter.Name = "buttonEnter";
+            this.buttonEnter.Size = new System.Drawing.Size(75, 23);
+            this.buttonEnter.TabIndex = 5;
+            this.buttonEnter.Text = "&Enter";
+            this.toolTip.SetToolTip(this.buttonEnter, "Click to attempt to enter the current units shipped");
+            this.buttonEnter.UseVisualStyleBackColor = true;
+            this.buttonEnter.Click += new System.EventHandler(this.buttonEnter_Click);
+            // 
+            // buttonReset
+            // 
+            this.buttonReset.Location = new System.Drawing.Point(107, 277);
+            this.buttonReset.Name = "buttonReset";
+            this.buttonReset.Size = new System.Drawing.Size(75, 23);
+            this.buttonReset.TabIndex = 6;
+            this.buttonReset.Text = "&Reset";
+            this.toolTip.SetToolTip(this.buttonReset, "Click to reset the form");
+            this.buttonReset.UseVisualStyleBackColor = true;
+            this.buttonReset.Click += new System.EventHandler(this.buttonReset_Click);
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Location = new System.Drawing.Point(203, 277);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(75, 23);
+            this.buttonExit.TabIndex = 7;
+            this.buttonExit.Text = "E&xit";
+            this.toolTip.SetToolTip(this.buttonExit, "Click to exit the application");
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // textBoxUnitsInput
+            // 
+            this.textBoxUnitsInput.Location = new System.Drawing.Point(95, 26);
+            this.textBoxUnitsInput.Name = "textBoxUnitsInput";
+            this.textBoxUnitsInput.Size = new System.Drawing.Size(100, 23);
+            this.textBoxUnitsInput.TabIndex = 1;
+            this.toolTip.SetToolTip(this.textBoxUnitsInput, "Enter the day\'s number of units shipped");
+            this.textBoxUnitsInput.TextChanged += new System.EventHandler(this.textBoxUnitsInput_TextChanged);
+            // 
+            // textBoxPastEntries
+            // 
+            this.textBoxPastEntries.BackColor = System.Drawing.SystemColors.ControlLightLight;
+            this.textBoxPastEntries.Location = new System.Drawing.Point(52, 55);
+            this.textBoxPastEntries.Multiline = true;
+            this.textBoxPastEntries.Name = "textBoxPastEntries";
+            this.textBoxPastEntries.ReadOnly = true;
+            this.textBoxPastEntries.Size = new System.Drawing.Size(187, 187);
+            this.textBoxPastEntries.TabIndex = 3;
+            this.toolTip.SetToolTip(this.textBoxPastEntries, "Displays the units shipped for each day");
+            // 
+            // labelAverage
+            // 
+            this.labelAverage.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.labelAverage.Location = new System.Drawing.Point(52, 245);
+            this.labelAverage.Name = "labelAverage";
+            this.labelAverage.Size = new System.Drawing.Size(187, 29);
+            this.labelAverage.TabIndex = 4;
+            this.labelAverage.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.toolTip.SetToolTip(this.labelAverage, "Displays the average of units shipped");
+            this.labelAverage.Click += new System.EventHandler(this.label1_Click_1);
+            // 
+            // formAverageUnitsShipped
+            // 
+            this.AcceptButton = this.buttonEnter;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.CancelButton = this.buttonReset;
+            this.ClientSize = new System.Drawing.Size(302, 312);
+            this.Controls.Add(this.labelAverage);
+            this.Controls.Add(this.textBoxPastEntries);
+            this.Controls.Add(this.textBoxUnitsInput);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.buttonReset);
+            this.Controls.Add(this.buttonEnter);
+            this.Controls.Add(this.labelDay);
+            this.Controls.Add(this.labelUnitsPrompt);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "formAverageUnitsShipped";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Average Units Shipped";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label labelUnitsPrompt;
+        private System.Windows.Forms.Label labelDay;
+        private System.Windows.Forms.Button buttonEnter;
+        private System.Windows.Forms.Button buttonReset;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.TextBox textBoxUnitsInput;
+        private System.Windows.Forms.TextBox textBoxPastEntries;
+        private System.Windows.Forms.Label labelAverage;
+        private System.Windows.Forms.ToolTip toolTip;
+    }
+}
+
---
+++ AverageUnitsShippedCalculator.cs
@@ -0,0 +1,162 @@
+﻿// Name: Stevenson Suhardy
+// Date: January 25, 2022
+// App name: Average Units Shipped Calculator
+// App description: Calculates the average units shipped for 7 days.
+
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace AverageUnitsShippedCalculator
+{
+    public partial class formAverageUnitsShipped : Form
+    {
+        public formAverageUnitsShipped()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBoxUnitsInput_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Closes the form or application
+        /// </summary>
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            // Closes the app
+            Close();
+        }
+        /// <summary>
+        /// Clears or reset the form to it's original state.
+        /// </summary>
+        private void buttonReset_Click(object sender, EventArgs e)
+        {
+            // Clear the form
+            textBoxPastEntries.Clear();
+            textBoxUnitsInput.Clear();
+            labelAverage.Text = string.Empty;
+            // Enabling the enter button after pressing reset
+            buttonEnter.Enabled = true;
+            // Changing the input textbox back to it's original state
+            textBoxUnitsInput.ReadOnly = false;
+            // Changing the label text back to Day 1
+            labelDay.Text = "Day 1";
+            // Changing the day value back to 2 or it's original value
+            day = 2;
+            // Changing the index back to it's original value
+            index = 0;
+            // Changing the average back to 0
+            average = 0;
+            // Clearing the array in order to insert another new 7 data inside the array
+            Array.Clear(listOfUnits, 0, listOfUnits.Length);
+            // Change the focus to the textBoxUnitsInput
+            textBoxUnitsInput.Focus();
+        }
+        // Declaring variables necessary for the application to work
+        int unit;
+        int day = 2;
+        double average;
+        int[] listOfUnits = new int[7];
+        int index = 0;
+        const int totalDays = 7;
+        const int maxNumberOfUnits = 5000;
+        const int minNumberOfUnits = 0;
+
+        /// <summary>
+        /// The user will type a data into the entry box, and first it will validate whether the data that the user inputs is
+        /// an integer or not and displays an error if it is not integer. Then, it will validate the data's range and see if it
+        /// is between 0 and 5000. If it is, then it will enter the data into a multiline box and after entering the data 7 
+        /// times, the enter button will be disabled and the average of the data input will be displayed as a label.
+        /// </summary>
+        private void buttonEnter_Click(object sender, EventArgs e)
+        {
+            // Checking if the input is an integer
+            if (int.TryParse(textBoxUnitsInput.Text, out unit))
+            {
+                // Checking if the input is between 0 and 5000
+                if (unit >= minNumberOfUnits && unit <= maxNumberOfUnits)
+                {
+                    // Adding the value to a variable in order to be calculated
+                    listOfUnits[index] = unit;
+                    // Incrementing index variable to indicate the array index
+                    index++;
+                    // Clears the input box so, the user can input another data
+                    textBoxUnitsInput.Text = string.Empty;
+                    // Converting the input into a string again in order to display it in a multiline box
+                    textBoxPastEntries.AppendText(Convert.ToString(unit));
+                    // Creating a new line
+                    textBoxPastEntries.AppendText("\r\n");
+                    // Checking if the day is still 7 or below
+                    if (day <= totalDays)
+                    {
+                        // Displaying the day number using the day variable
+                        labelDay.Text = String.Format("Day {0}", day);
+                        // Incrementing the day by one everytime it is still under 7
+                        day++;
+                    }
+                    // If the day number is more than 7
+                    else
+                    {
+                        // Changing the input textbox to readonly
+                        textBoxUnitsInput.ReadOnly = true;
+                        // Disabling the enter button
+                        buttonEnter.Enabled = false;
+                        // Iterating through the array and adding each data inside the array into a variable
+                        foreach(var totalUnit in listOfUnits)
+                        {
+                            average += totalUnit;
+                        }
+                        // Calculating the average of units shipped or the data entered by the user
+                        average /= totalDays;
+                        // Rounding the result to 2 decimal places
+                        average = Math.Round(average, 2);
+                        // Displaying the results in a label
+                        labelAverage.Text = String.Format("Average: {0}", average);
+                    }
+                }
+                // If the input is not inside the valid range
+                else
+                {
+                    // Display the following messagebox
+                    MessageBox.Show(String.Format("Please enter a number {0} between and {1}", minNumberOfUnits, 
+                        maxNumberOfUnits), "INVALID INPUT RANGE", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    // Change the focus to the input textbox
+                    textBoxUnitsInput.Focus();
+                }
+            }
+            // If the input is not a whole number
+            else
+            {
+                // Display the following messagebox
+                MessageBox.Show("Please enter a whole number", "INVALID INPUT DATA TYPE", MessageBoxButtons.OK, 
+                    MessageBoxIcon.Error);
+                // Change the focus to the input textbox
+                textBoxUnitsInput.Focus();
+            }
+            
+        }
+    }
+}
